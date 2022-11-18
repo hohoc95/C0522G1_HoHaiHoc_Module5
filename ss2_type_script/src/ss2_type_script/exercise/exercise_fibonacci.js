12d929ca053fded97d75c1cd0b181a7987fce66f
unknown <you@example.com>
2022-11-18 15:56:56 +0700

    update

--- ss2_type_script/src/ss2_type_script/exercise/exercise_fibonacci.js
+++ ss2_type_script/src/ss2_type_script/exercise/exercise_fibonacci.js
@@ -1,18 +1,18 @@
-var checkFibonacci = function (num) {
+var fibonacci = function (num) {
     if (num == 0) {
         return 0;
     }
     else if (num == 1) {
         return 1;
     }
-    return checkFibonacci(num - 1) + checkFibonacci(num - 2);
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 var sums = 0;
 var size = 3;
 var arrr = [];
 for (var i = 0; i < size; i++) {
-    arrr.push(checkFibonacci(i));
-    sums += checkFibonacci(i);
+    arrr.push(fibonacci(i));
+    sums += fibonacci(i);
 }
 console.log("Dãy fibonacci: " + arrr);
 console.log("Tổng " + size + " số fibonacci: " + sums);
--- ss2_type_script/src/ss2_type_script/exercise/exercise_fibonacci.ts
+++ ss2_type_script/src/ss2_type_script/exercise/exercise_fibonacci.ts
@@ -1,22 +1,21 @@
 
-let checkFibonacci = function (num: number): number {
+let fibonacci = function (num: number): number {
     if (num == 0) {
         return 0;
     }
-
     else if (num == 1) {
         return 1;
     }
 
-    return checkFibonacci(num - 1) + checkFibonacci(num - 2);
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 let sums: number = 0;
 let size: number = 3;
-let arrr: Array<number> = [];
+let arrr: Array <number> = [];
 for (let i = 0; i < size; i++) {
-    arrr.push(checkFibonacci(i))
-    sums += checkFibonacci(i);
+    arrr.push(fibonacci(i))
+    sums += fibonacci(i);
 }
 
 console.log("Dãy fibonacci: " + arrr);
