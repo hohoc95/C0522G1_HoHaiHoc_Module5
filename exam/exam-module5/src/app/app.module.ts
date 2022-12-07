67ab6e248d78ce8ac24e3dd61212c0db3ff1bcec
unknown <you@example.com>
2022-12-07 15:28:55 +0700

    Finished

--- exam/exam-module5/src/app/app.module.ts
+++ exam/exam-module5/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TicketListComponent } from './ticket/ticket-list/ticket-list.component';
-import { TicketCreateComponent } from './ticket/ticket-create/ticket-create.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TicketListComponent} from './ticket/ticket-list/ticket-list.component';
+import {TicketCreateComponent} from './ticket/ticket-create/ticket-create.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { TicketEditComponent } from './ticket/ticket-edit/ticket-edit.component';
+import {TicketEditComponent} from './ticket/ticket-edit/ticket-edit.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +16,15 @@ import { TicketEditComponent } from './ticket/ticket-edit/ticket-edit.component'
     TicketCreateComponent,
     TicketEditComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HttpClientModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
