cde498465ff33448a50ab6091b533dda3b6b8d4d
unknown <you@example.com>
2023-01-08 22:53:39 +0700

    final

--- md5exam/exam-md5/src/app/service/repo.service.ts
+++ md5exam/exam-md5/src/app/service/repo.service.ts
@@ -4,39 +4,42 @@ import {Observable} from "rxjs";
 import {Repo} from "../model/repo";
 import {Product} from "../model/product";
 
+const API_URL = 'http://localhost:8080/api';
+
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
-  private API_URL= 'http://localhost:3000/';
+  // private API_URL= 'http://localhost:3000/';
   constructor(private httpClient: HttpClient) { }
 
   findAllRepoSearch(nameSearch: string, dateInSearch: string, dateEndSearch: string): Observable<Repo[]>{
-    return this.httpClient.get<Repo[]>('http://localhost:3000/'+ 'repo?repoDateIn_like=' + dateInSearch + '&repoDateEnd_like=' + dateEndSearch + '&product.productName_like=' + nameSearch);
+    return this.httpClient.get<Repo[]>(API_URL+ 'repo?repoDateIn_like=' + dateInSearch + '&repoDateEnd_like=' + dateEndSearch + '&product.productName_like=' + nameSearch);
   }
 
   findRepoSearchPaging(numberRecord: number, curPage: number, nameSearch: string, dateInSearch: string, dateEndSearch: string): Observable<Repo[]>{
-    return this.httpClient.get<Repo[]>('http://localhost:3000/' + 'repo?_page=' + curPage + '&_limit=' + numberRecord + '&repoDateIn_like=' + dateInSearch + '&repoDateEnd_like=' + dateEndSearch + '&product.productName_like=' + nameSearch);
+    return this.httpClient.get<Repo[]>(API_URL + 'repo?_page=' + curPage + '&_limit=' + numberRecord + '&repoDateIn_like=' + dateInSearch + '&repoDateEnd_like=' + dateEndSearch + '&product.productName_like=' + nameSearch);
   }
 
+
   findAllProduct(): Observable<Product[]>{
-    return this.httpClient.get<Product[]>('http://localhost:3000/' + 'product');
+    return this.httpClient.get<Product[]>(API_URL + 'product');
   }
 
   deteleGame(id: number): Observable<Repo> {
-    return this.httpClient.delete<Repo>('http://localhost:3000/' + 'repo/' + id)
+    return this.httpClient.delete<Repo>(API_URL + 'repo/' + id)
   }
 
   getById(id: number): Observable<Repo> {
-    return this.httpClient.get<Repo>('http://localhost:3000/' + 'repo/' + id)
+    return this.httpClient.get<Repo>(API_URL + 'repo/' + id)
   }
 
   addRepo(repo): Observable<Repo>{
-    return this.httpClient.post<Repo>('http://localhost:3000/' + 'repo', repo);
+    return this.httpClient.post<Repo>(API_URL + 'repo', repo);
   }
 
   updateRepo(id: number, repo:Repo): Observable<Repo>{
-    return this.httpClient.put<Repo>(this.API_URL + 'repo/' + id, repo)
+    return this.httpClient.put<Repo>(API_URL + 'repo/' + id, repo)
   }
 
 }
